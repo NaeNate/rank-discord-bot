fc4bf888ba582c695b52cf266f7aca5387f7737e
Nate <nate@qstreet.org>
2020-12-28 00:01:26 -0500

    Setup rank up and down

---
+++ index-orig.js
@@ -0,0 +1,153 @@
+// const Discord = require("discord.js");
+// require("dotenv").config();
+// const client = new Discord.Client();
+// const botToken = process.env.BOT_TOKEN;
+// const prefix = "!";
+// const rankRoles = [
+//   "Iron 1",
+//   "Iron 2",
+//   "Iron 2",
+//   "Bronze 1",
+//   "Bronze 2",
+//   "Bronze 3",
+//   "Silver 1",
+//   "Silver 2",
+//   "Silver 3",
+// ];
+// //Start the bot
+// client.login(botToken);
+
+// //Once bot is ready, log
+// client.on("ready", () => {
+//   console.log("Online");
+// });
+
+// client.on("message", (message) => {
+//   if (!message.content.startsWith(prefix) || message.author.bot) {
+//     return;
+//   }
+//   const command = message.content.slice(1).toLowerCase().split(" ")[0];
+
+//   if (command === "rank") {
+//     changeRank(message);
+//   } else if (command === "rankup") {
+//     rankUp(message)
+//   } else if (command === "rankdown") {
+//     rankDown(message)
+//   }
+// });
+
+// const changeRank = (message) => {
+//   const member = message.guild.members.cache.get(message.author.id);
+//   const role = message.guild.roles.cache.find((role) => {
+//     return role.name === message.content.slice(6);
+//   });
+//   console.log(role)
+//   if (
+//     role 
+//     // &&
+//     // role.name != "Admin" &&
+//     // role.name != "Queen" &&
+//     // role.name != "Coach" &&
+//     // role.name != "betrayer" &&
+//     // role.name != "luna's simp" &&
+//     // role.name != "gamer girl" &&
+//     // role.name != "Mod" &&
+//     // role.name != "asshole" &&
+//     // role.name != "Annoying" &&
+//     // role.name != "slice" &&
+//     // role.name != "adiadiadiadi" &&
+//     // role.name != "Rank Bot" &&
+//     // role.name != "Rythm" &&
+//     // role.name != "Bots" &&
+//     // role.name != "MEE6"
+//   ) {
+//     rankRoles.forEach((roleName) => {
+//       const realRole = message.guild.roles.cache.find((role) => {
+//         return role.name === roleName;
+//       });
+//       member.roles.remove(realRole);
+//     });
+//     member.roles.add(role);
+//     message.reply("Got it.");
+//   } else {
+//     message.reply("Please list a valid rank. Example: Iron 1, Bronze 2");
+//   }
+// };
+
+// const rankUp = (message) => {
+//   const member = message.guild.members.cache.get(message.author.id);
+//   const role = message.guild.roles.cache.find((role) => {
+//     return role.name === message.content.slice(6);
+//   });
+//   console.log(role)
+//   if (
+//     role 
+//     // &&
+//     // role.name != "Admin" &&
+//     // role.name != "Queen" &&
+//     // role.name != "Coach" &&
+//     // role.name != "betrayer" &&
+//     // role.name != "luna's simp" &&
+//     // role.name != "gamer girl" &&
+//     // role.name != "Mod" &&
+//     // role.name != "asshole" &&
+//     // role.name != "Annoying" &&
+//     // role.name != "slice" &&
+//     // role.name != "adiadiadiadi" &&
+//     // role.name != "Rank Bot" &&
+//     // role.name != "Rythm" &&
+//     // role.name != "Bots" &&
+//     // role.name != "MEE6"
+//   ) {
+//     rankRoles.forEach((roleName) => {
+//       const realRole = message.guild.roles.cache.find((role) => {
+//         return role.name === roleName;
+//       });
+//       member.roles.remove(realRole);
+//     });
+//     member.roles.add(role);
+//     message.reply("Got it.");
+//   } else {
+//     message.reply("Please list a valid rank. Example: Iron 1, Bronze 2");
+//   }
+// }
+
+// const rankDown = (message) => {
+//   const member = message.guild.members.cache.get(message.author.id);
+//   const role = message.guild.roles.cache.find((role) => {
+//     return role.name === message.content.slice(6);
+//   });
+//   console.log(role)
+//   if (
+//     role 
+//     // &&
+//     // role.name != "Admin" &&
+//     // role.name != "Queen" &&
+//     // role.name != "Coach" &&
+//     // role.name != "betrayer" &&
+//     // role.name != "luna's simp" &&
+//     // role.name != "gamer girl" &&
+//     // role.name != "Mod" &&
+//     // role.name != "asshole" &&
+//     // role.name != "Annoying" &&
+//     // role.name != "slice" &&
+//     // role.name != "adiadiadiadi" &&
+//     // role.name != "Rank Bot" &&
+//     // role.name != "Rythm" &&
+//     // role.name != "Bots" &&
+//     // role.name != "MEE6"
+//   ) {
+//     rankRoles.forEach((roleName) => {
+//       const realRole = message.guild.roles.cache.find((role) => {
+//         return role.name === roleName;
+//       });
+//       member.roles.remove(realRole);
+//     });
+//     member.roles.add(role);
+//     message.reply("Got it.");
+//   } else {
+//     message.reply("Please list a valid rank. Example: Iron 1, Bronze 2");
+//   }
+// }
+
--- index.js
+++ index.js
@@ -3,10 +3,14 @@ require("dotenv").config();
 const client = new Discord.Client();
 const botToken = process.env.BOT_TOKEN;
 const prefix = "!";
-const rankRoles = [
+
+let rank;
+let rankNum;
+
+const ranks = [
   "Iron 1",
   "Iron 2",
-  "Iron 2",
+  "Iron 3",
   "Bronze 1",
   "Bronze 2",
   "Bronze 3",
@@ -14,49 +18,76 @@ const rankRoles = [
   "Silver 2",
   "Silver 3",
 ];
-//Start the bot
-client.login(botToken);
 
-//Once bot is ready, log
-client.on("ready", () => {
-  console.log("Online");
-});
+client.login(botToken);
+client.on("ready", () => console.log("Online"));
 
 client.on("message", (message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) {
     return;
   }
-  const command = message.content.slice(1).toLowerCase().split(" ")[0];
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
 
   if (command === "rank") {
-    changeRank(message);
+    setRank(message, command);
+  } else if (command === "rankup") {
+    rankUp(message);
+  } else if (command === "rankdown") {
+    rankDown(message);
   }
 });
 
-const changeRank = (message) => {
+const setRank = (message, command) => {
+  const roleArg = message.content.slice(prefix.length + command.length + 1);
+
+  ranks.forEach(function (role, index) {
+    if (role === roleArg) {
+      rank = ranks[index];
+      rankNum = ranks.indexOf(rank);
+    }
+  });
+  changeRank(message);
+};
+
+const rankUp = (message) => {
+  rank = ranks[rankNum + 1];
+  rankNum = ranks.indexOf(rank);
+  changeRank(message, true);
+};
+
+const rankDown = (message) => {
+  rank = ranks[rankNum - 1];
+  rankNum = ranks.indexOf(rank);
+  changeRank(message, true);
+};
+
+const changeRank = (message, optional) => {
   const member = message.guild.members.cache.get(message.author.id);
+
   const role = message.guild.roles.cache.find((role) => {
-    return role.name === message.content.slice(6);
+    return role.name === rank;
   });
   if (
-    role &&
-    role.name != "Admin" &&
-    role.name != "Queen" &&
-    role.name != "Coach" &&
-    role.name != "betrayer" &&
-    role.name != "luna's simp" &&
-    role.name != "gamer girl" &&
-    role.name != "Mod" &&
-    role.name != "asshole" &&
-    role.name != "Annoying" &&
-    role.name != "slice" &&
-    role.name != "adiadiadiadi" &&
-    role.name != "Rank Bot" &&
-    role.name != "Rythm" &&
-    role.name != "Bots" &&
-    role.name != "MEE6"
+    (rank || role || optional) &&
+    rank != "Admin" &&
+    rank != "Queen" &&
+    rank != "Coach" &&
+    rank != "betrayer" &&
+    rank != "luna's simp" &&
+    rank != "gamer girl" &&
+    rank != "Mod" &&
+    rank != "asshole" &&
+    rank != "Annoying" &&
+    rank != "slice" &&
+    rank != "adiadiadiadi" &&
+    rank != "Rank Bot" &&
+    rank != "Rythm" &&
+    rank != "Bots" &&
+    rank != "MEE6"
   ) {
-    rankRoles.forEach((roleName) => {
+    ranks.forEach((roleName) => {
       const realRole = message.guild.roles.cache.find((role) => {
         return role.name === roleName;
       });
